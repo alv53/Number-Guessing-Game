32b620d8a4790fd25a933e6f2d53ecb21f363714
alv53 <alvjou@gmail.com>
2013-10-12 13:19:27 -0500

    set up menu frame

--- Number_Guessing_Game/src/mainMenu.java
+++ Number_Guessing_Game/src/mainMenu.java
@@ -1,12 +1,41 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class mainMenu {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+public class mainMenu extends JFrame {
+
+	private JPanel contentPane;
 
 	/**
-	 * @param args
+	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(numberFunctions.calcScore(100,4));
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mainMenu frame = new mainMenu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public mainMenu() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 	}
 
 }
--- Number_Guessing_Game/src/numberFunctions.java
+++ Number_Guessing_Game/src/numberFunctions.java
@@ -1,6 +1,14 @@
 
 public class numberFunctions {
+	//This function will calculate the player's score after they complete the game
 	public static int calcScore(int time, int guesses){
 		return (100*time)/guesses;
+	}
+	//This function generates a random number with unique digits, and cannot start with 0
+	public static String generateNumber(int digits){
+		return "1234";
+	}
+	public static boolean checkGuess(String guess, String answer){
+		return true;
 	}
 }
