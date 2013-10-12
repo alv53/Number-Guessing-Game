36f46435c7592dc9b1791a7966488660066c2e2e
alv53 <alvjou@gmail.com>
2013-10-12 12:50:26 -0500

    created some base files

---
+++ Number_Guessing_Game/src/mainMenu.java
@@ -0,0 +1,12 @@
+
+public class mainMenu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Number_Guessing_Game/src/playerScore.java
@@ -0,0 +1,11 @@
+
+public class playerScore {
+	private String player;
+	private int score;
+	public String getName(){
+		return player;
+	}
+	public int getScore(){
+		return score;
+	}
+}
