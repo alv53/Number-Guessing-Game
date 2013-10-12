5dcb1c25814f3186d1031382f7384e3e62476270
alv53 <alvjou@gmail.com>
2013-10-12 12:54:15 -0500

    score generator

--- Number_Guessing_Game/src/mainMenu.java
+++ Number_Guessing_Game/src/mainMenu.java
@@ -6,7 +6,7 @@ public class mainMenu {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(numberFunctions.calcScore(100,4));
 	}
 
 }
--- Number_Guessing_Game/src/numberFunctions.java
+++ Number_Guessing_Game/src/numberFunctions.java
@@ -1,4 +1,6 @@
 
 public class numberFunctions {
-	
+	public static int calcScore(int time, int guesses){
+		return (100*time)/guesses;
+	}
 }
